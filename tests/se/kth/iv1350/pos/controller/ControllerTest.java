e9810b8622df14c6b76103f8d6f0965bc7c00c2d
Jakob Olsson <jakobolsson973@gmail.com>
2021-04-27 19:15:59 +0200

    Generated tests for Controller

---
+++ tests/se/kth/iv1350/pos/controller/ControllerTest.java
@@ -0,0 +1,26 @@
+package se.kth.iv1350.pos.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void startSale() {
+    }
+
+    @Test
+    void addItemToSale() {
+    }
+}
