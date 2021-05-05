a55be1f8f7e535f553682489aad791144f22fa2c
Jakob Olsson <jakobolsson973@gmail.com>
2021-05-05 12:47:15 +0200

    Added tests for controller

--- tests/se/kth/iv1350/pos/controller/ControllerTest.java
+++ tests/se/kth/iv1350/pos/controller/ControllerTest.java
@@ -10,8 +10,7 @@ import se.kth.iv1350.pos.integration.Inventory;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-class ControllerTest { // Ska jag verkligen göra tester för controller? Det känns som jag bara checkar alla
-                        // saker som redan checkas av Sale själv.
+class ControllerTest {
 
     //////////////////////////////
     //          SETUP           //
@@ -41,7 +40,7 @@ class ControllerTest { // Ska jag verkligen göra tester för controller? Det k�
     //          addItemToSale()         //
     //////////////////////////////////////
     @Test
-    void addItemToSaleItemDTO() {
+    void addItemToSaleTestItemDTO() {
         controller.startSale();
         ItemDTO expectedResult = new Inventory().getItemInfo(1);
         SaleDTO saleDTO = controller.addItemToSale(1, 2);
@@ -50,7 +49,7 @@ class ControllerTest { // Ska jag verkligen göra tester för controller? Det k�
     }
 
     @Test
-    void addItemToSaleQuantity() {
+    void addItemToSaleTestQuantity() {
         controller.startSale();
         int expectedResult = 2;
         SaleDTO saleDTO = controller.addItemToSale(1, 2);
@@ -59,7 +58,7 @@ class ControllerTest { // Ska jag verkligen göra tester för controller? Det k�
     }
 
     @Test
-    void addItemToSaleRunningTotalIncludingVAT() {
+    void addItemToSaleTestRunningTotalIncludingVAT() {
         controller.startSale();
         double expectedResult = 2 * 25 * 1.2;
         SaleDTO saleDTO = controller.addItemToSale(1, 2);
@@ -71,20 +70,122 @@ class ControllerTest { // Ska jag verkligen göra tester för controller? Det k�
     //          endRegistering()            //
     //////////////////////////////////////////
     @Test
-    void endRegistering() {
+    void endRegisteringTestTotalPriceOneItem() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        SaleDTO saleDTO = controller.endRegistering();
+        double expectedResult = 25 * 1.2;
+        double actualResult = saleDTO.getPaymentInformation().getTotalPrice();
+        assertEquals(expectedResult, actualResult, "Total Price was not calculated correctly");
+    }
+
+    @Test
+    void endRegisteringTestTotalPriceTwoDifferentItems() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        controller.addItemToSale(2, 1);
+        SaleDTO saleDTO = controller.endRegistering();
+        double expectedResult = 25 * 1.2 + 20 * 1.3;
+        double actualResult = saleDTO.getPaymentInformation().getTotalPrice();
+        assertEquals(expectedResult, actualResult, "Total Price was not calculated correctly");
+    }
+
+    @Test
+    void endRegisteringTestTotalVATOneItem() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        SaleDTO saleDTO = controller.endRegistering();
+        double expectedResult = 25 * 0.2;
+        double actualResult = saleDTO.getPaymentInformation().getTotalVAT();
+        assertEquals(expectedResult, actualResult, "Total VAT was not calculated correctly");
+    }
+
+    @Test
+    void endRegisteringTestTotalVATTwoDifferentItems() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        controller.addItemToSale(2, 1);
+        SaleDTO saleDTO = controller.endRegistering();
+        double expectedResult = 25 * 0.2 + 20 * 0.3;
+        double actualResult = saleDTO.getPaymentInformation().getTotalVAT();
+        assertEquals(expectedResult, actualResult, "Total VAT was not calculated correctly");
     }
 
     ////////////////////////////////////////
     //          discountRequest()         //
     ////////////////////////////////////////
     @Test
-    void discountRequest() {
+    void discountRequestTestTotalPriceValid() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        controller.endRegistering();
+        int eligibleCustomer = 123;
+        SaleDTO saleDTO = controller.discountRequest(eligibleCustomer);
+        double expectedResult = 25 * 1.2 / 2;
+        double actualResult = saleDTO.getPaymentInformation().getTotalPrice();
+        assertEquals(expectedResult, actualResult, "Total Price was not correctly modified by discount");
+    }
+
+    @Test
+    void discountRequestTestTotalVATValid() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        controller.endRegistering();
+        int eligibleCustomer = 123;
+        SaleDTO saleDTO = controller.discountRequest(eligibleCustomer);
+        double expectedResult = 25 * 0.2 / 2;
+        double actualResult = saleDTO.getPaymentInformation().getTotalVAT();
+        assertEquals(expectedResult, actualResult, "Total VAT was not correctly modified by discount");
+    }
+
+    @Test
+    void discountRequestTestTotalPriceInvalid() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        controller.endRegistering();
+        int ineligibleCustomer = 1;
+        SaleDTO saleDTO = controller.discountRequest(ineligibleCustomer);
+        double expectedResult = 25 * 1.2;
+        double actualResult = saleDTO.getPaymentInformation().getTotalPrice();
+        assertEquals(expectedResult, actualResult, "Total Price was not correctly modified by discount");
+    }
+
+    @Test
+    void discountRequestTestTotalVATInvalid() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        controller.endRegistering();
+        int ineligibleCustomer = 1;
+        SaleDTO saleDTO = controller.discountRequest(ineligibleCustomer);
+        double expectedResult = 25 * 0.2;
+        double actualResult = saleDTO.getPaymentInformation().getTotalVAT();
+        assertEquals(expectedResult, actualResult, "Total VAT was not correctly modified by discount");
     }
 
     //////////////////////////////////////
     //          addPayment()            //
     //////////////////////////////////////
     @Test
-    void addPayment() {
+    void addPaymentTestAmountPaid() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        controller.endRegistering();
+        double amountPaid = 1000;
+        SaleDTO saleDTO = controller.addPayment(amountPaid);
+        double expected = 1000;
+        double actual = saleDTO.getPaymentInformation().getAmountPaid();
+        assertEquals(expected, actual, "AmountPaid was not added correctly");
+    }
+
+    @Test
+    void addPaymentTestChange() {
+        controller.startSale();
+        controller.addItemToSale(1, 1);
+        controller.endRegistering();
+        double amountPaid = 1000;
+        SaleDTO saleDTO = controller.addPayment(amountPaid);
+        double expected = amountPaid - 25 * 1.2;
+        double actual = saleDTO.getPaymentInformation().getChange();
+        assertEquals(expected, actual, "Change was not calculated correctly");
     }
 }
